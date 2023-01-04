26063dbcbf5ed4072ba47c0c4757079d5efda7af
Valeriy Chechin <valerachechin@gmail.com>
2023-01-04 21:32:58 +0100

    Add 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,7 @@
 const navCtgrs = document.querySelectorAll(".item");
 console.log("Number of categories:", navCtgrs.length);
 
-const navEl = document.querySelector("ul");
-console.log(navEl);
+const navEl = document.querySelectorAll("h2");
+console.log(navEl.length);
+
+console.log("Category:", navEl[0].textContent);
