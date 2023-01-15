03e4a4424cc9f32d5b0b8c5b905e121afac675ab
Valeriy Chechin <valerachechin@gmail.com>
2023-01-15 12:03:12 +0100

    1234

--- js/task-01.js
+++ js/task-01.js
@@ -1,14 +1,20 @@
-const navCtgrs = document.querySelectorAll(".item");
-console.log("Number of categories:", navCtgrs.length);
+// const navCtgrs = document.querySelectorAll(".item");
+// console.log("Number of categories:", navCtgrs.length);
 
-const navEl = document.querySelectorAll("h2");
-console.log(navEl);
+// const navEl = document.querySelectorAll("h2");
 
-navEl.forEach(function (number, index) {
-  console.log("Category:", navEl[index].textContent);
-  const navElItem = document.querySelector("ul");
-  console.log(navElItem);
-});
+// navEl.forEach(function (number, index) {
+//   console.log("Category:", navEl[index].textContent);
+//   const navElItem = document.querySelector("li");
+// });
 
-const navElem = document.getElementsByClassName("item");
-console.log(navElem);
+const totalCategories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${totalCategories.length}`);
+
+const categoriesArray = [...totalCategories]
+  .map(
+    (categories) => `Category: ${categories.children[0].textContent}
+Elements: ${categories.children[1].children.length}`
+  )
+  .join("\n");
+console.log(categoriesArray);
